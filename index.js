3e7085133cfec495f4f6d09d3f69cb8280a4b4eb
arsalan zahoor <arsalan.zahoor@esajeesolutions.com>
2014-12-26 17:11:54 +0500

    Task 2.4 Using Polymer and Oauth2-Server with Project(Uptp Home Page)

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 var express = require('express'),
-  bodyParser = require('body-parser'),
-  oauthserver = require('node-oauth2-server'); // Would be: 'oauth2-server'
+bodyParser = require('body-parser'),
+oauthserver = require('node-oauth2-server'); // Would be: 'oauth2-server'
 
 var app = express();
 app.use(logErrors);
 app.use(express.static(__dirname + '/public'));
 function logErrors(err, req, res, next) {
-  console.error(err.stack);
-  next(err);
+    console.error(err.stack);
+    next(err);
 }
 app.use(bodyParser());
 
 app.oauth = oauthserver({
-  model: require('./model'),
-  grants: ['auth_code', 'password'],
-  debug: true
+    model: require('./model'),
+    grants: ['auth_code', 'password'],
+    debug: true
 });
 
 // Handle token grant requests
@@ -22,76 +22,156 @@ app.all('/oauth/token', app.oauth.grant());
 
 // Show them the "do you authorise xyz app to access your content?" page
 app.get('/oauth/authorise', function (req, res, next) {
-  if (!req.session.user) {
-    // If they aren't logged in, send them to your own login implementation
-    return res.redirect('/login?redirect=' + req.path + '&client_id=' +
-        req.query.client_id + '&redirect_uri=' + req.query.redirect_uri);
-  }
+    if (!req.session.user) {
+        // If they aren't logged in, send them to your own login implementation
+        return res.redirect('/login?redirect=' + req.path + '&client_id=' +
+            req.query.client_id + '&redirect_uri=' + req.query.redirect_uri);
+    }
 
-  res.render('authorise', {
-    client_id: req.query.client_id,
-    redirect_uri: req.query.redirect_uri
-  });
+    res.render('authorise', {
+        client_id: req.query.client_id,
+        redirect_uri: req.query.redirect_uri
+    });
 });
 
 // Handle authorise
 app.post('/oauth/authorise', function (req, res, next) {
-  if (!req.session.user) {
-    return res.redirect('/login?client_id=' + req.query.client_id +
-      '&redirect_uri=' + req.query.redirect_uri);
-  }
+    if (!req.session.user) {
+        return res.redirect('/login?client_id=' + req.query.client_id +
+            '&redirect_uri=' + req.query.redirect_uri);
+    }
 
-  next();
+    next();
 }, app.oauth.authCodeGrant(function (req, next) {
-  // The first param should to indicate an error
-  // The second param should a bool to indicate if the user did authorise the app
-  // The third param should for the user/uid (only used for passing to saveAuthCode)
-  next(null, req.body.allow === 'yes', req.session.user.id, req.session.user);
+    // The first param should to indicate an error
+    // The second param should a bool to indicate if the user did authorise the app
+    // The third param should for the user/uid (only used for passing to saveAuthCode)
+    next(null, req.body.allow === 'yes', req.session.user.id, req.session.user);
 }));
 
 // Show login
 app.get('/signin', function (req, res, next) {
-  res.render('signin', {
-    redirect: req.query.redirect,
-    client_id: req.query.client_id,
-    redirect_uri: req.query.redirect_uri
-  });
+    res.render('signin', {
+        redirect: req.query.redirect,
+        client_id: req.query.client_id,
+        redirect_uri: req.query.redirect_uri
+    });
 });
 
 // Handle login
 app.post('/signin', function(req, res){
+    console.log(req.body);
     var username=req.body.username;
     var password=req.body.password;
+    res.send({status:true});
+        return;
     if(username != null && password != null)
-        {
-    var sql= 'select * from users where username= ? and password=?';
-    
-    var connection = require('./mysqlconnection.js');
-    
-    //if(username.length>1 || password.length>1)
-    //{
-    console.log( req.body);//display user input values on server side
-    connection.query(sql, [username,password],function(err, result)
     {
-        console.log(result);
-        if(result=='')
+        var sql= 'select * from users where username= ? and password=?';
+    
+        var connection = require('./mysqlconnection.js');
+    
+        //if(username.length>1 || password.length>1)
+        //{
+        //    console.log( req.body);//display user input values on server side
+        connection.query(sql, [username,password],function(err, result)
         {
-            res.redirect('/signin');
-        }
-        else
-        {
-            res.redirect('/home'); 
-        //res.sendFile(_dirname+'/SignIn.html')
-        }
-    }
-    );
-        }
-        else
+            console.log(result);
+            if(result=='')
             {
-                console.log("Username or Password Field Is Empty");
+                //            res.send({status:true});
+                console.log("Unauthorise User Values");
+            // res.redirect('/signin');
             }
+            else
+            {
+                res.send({status:true});
+
+                console.log("Authorised");
+            //res.redirect('/home'); 
+            //res.sendFile(_dirname+'/SignIn.html')
+            }
+        }
+        );
+    }
+    else
+    {
+        console.log("Username or Password Field Is Empty");
+    }
 //connection.end();
 });
+
+
+app.get('/home', function (req, res, next) {
+    if (!req.session.user) {
+        // If they aren't logged in, send them to your own login implementation
+        return res.redirect('/login?redirect=' + req.path + '&client_id=' +
+            req.query.client_id + '&redirect_uri=' + req.query.redirect_uri);
+    }
+
+    res.render('home', {
+        client_id: req.query.client_id,
+        redirect_uri: req.query.redirect_uri
+    });
+});
+
+
+       
+app.post('/home', function (req, res){
+    console.log(req.body);
+    var connection=require('./mysqlconnection.js')
+    var sqlquery;
+    var newusername=req.body.newusername;
+    var newpassword=req.body.newpassword;
+        
+    if(newusername != null && newpassword != null)
+    {
+        if(req.body.action=='insert')
+        {
+            sqlquery='insert into users (username,password) values('+connection.escape(newusername)+','+connection.escape(newpassword)+')';
+        //console.log("Congratulations");
+        //res.redirect('/Home');
+        }
+        else if(req.body.action=='update')
+        {
+            sqlquery='update users set password='+connection.escape(newpassword)+'where username='+ connection.escape(newusername);
+        //+connection.escape(newusername);    
+        //+connection.escape(newpassword)+'
+        }
+        else if(req.body.action=='delete')
+        {
+            sqlquery='delete from users where username='+connection.escape(newusername)+'and password='+connection.escape(newpassword);
+        }
+        else{
+            res.send("Wrong Action Input");
+        }
+        
+        connection.query(sqlquery,function(err,result)
+        {
+            console.log(result);
+            if(result==' ')
+            {
+                    
+                res.send('Database is not updated! Please put right information!')
+            }
+            else
+            {
+                //console.log(result);
+                res.send('Database is Updated!')
+            }
+    
+        })
+    }
+    else 
+    {
+        console.log("Username or Password field is empty");
+        res.send("Username or Password field is empty");
+    }
+//res.send("Congratulatios!!! You Have Successfully Login!"); 
+//res.redirect('/Home');
+// connection.end();
+});
+      
 
 /* Old Login Post
 app.post('/login', function (req, res, next) {
@@ -112,14 +192,14 @@ app.post('/login', function (req, res, next) {
 });
 */
 app.get('/', function (req, res) {
-  // Will require a valid access_token
-  //console.log("hello!");
-  res.sendFile(__dirname + '/public/pages/index.html');
+    // Will require a valid access_token
+    //console.log("hello!");
+    res.sendFile(__dirname + '/public/pages/index.html');
 });
 
 app.get('/public', function (req, res) {
-  // Does not require an access_token
-  res.send('Public area');
+    // Does not require an access_token
+    res.send('Public area');
 });
 
 // Error handling
